2d5bc3417c4398e96f8f0be096aaf3e977643763
davenda <42378597+davenda@users.noreply.github.com>
2021-09-30 21:28:49 -0500

    added assignment 4

---
+++ Assignments/Assign04/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const routes = require("./api/routes");
+const conn = require('./api/data/dbconnection');
+conn.open();
+
+const app = express();
+
+app.set('port', 3000);
+app.use(function(req, res, next){
+    console.log(req.url);
+    next();
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}))
+app.use('/api/', routes)
+
+const server = app.listen(app.get('port'), function(){
+    console.log("Listening to port ", server.address().port);
+});
